88f912de9240a6ce9b0a7e6c8f083b68e6c7d572
DylanBelka <dylan.belka@gmail.com>
2015-09-09 16:27:53 -0400

    Removed moved code

--- src/cpu.h
+++ src/cpu.h
@@ -1,14 +1,6 @@
 #ifndef GBEMC_CPU_H
 #define GBEMC_CPU_H
 
-struct instruction
-{
-	char *disassembly;
-	void *execute;
-	int opcode_length;
-	//int ticks;
-} extern const instructions[256];
-
 void reset_cpu(void);
 void cpu_tick(void);
 
