dfa43fc735dbcde1f4c92aa147916c900cd9f11e
DylanBelka <dylan.belka@gmail.com>
2015-09-09 15:23:22 -0400

    Added F registers raw value and removed one struct layer

--- src/registers.h
+++ src/registers.h
@@ -5,63 +5,58 @@
 
 struct registers
 {
-	struct
+	union
 	{
-		union
+		struct
 		{
 			struct
 			{
-				struct
+				union
 				{
-					u8 C : 1;
-					u8 N : 1;
-					u8 P : 1;
-					u8 F3 : 1;
-					u8 H : 1;
-					u8 F5 : 1;
-					u8 Z : 1;
-					u8 S : 1;
-				} F;
-				u8 A;
-			};
-			u16 AF;
+					struct
+					{
+						u8 C : 1;
+						u8 N : 1;
+						u8 P : 1;
+						u8 F3 : 1;
+						u8 H : 1;
+						u8 F5 : 1;
+						u8 Z : 1;
+						u8 S : 1;
+					};
+					u8 raw;
+				};
+			} F;
+			u8 A;
 		};
+		u16 AF;
 	};
-	struct
+	union
 	{
-		union
+		struct
 		{
-			struct
-			{
-				u8 C;
-				u8 B;
-			};
-			u16 BC;
+			u8 C;
+			u8 B;
 		};
+		u16 BC;
 	};
-	struct
+	union 
 	{
-		union 
+		struct
 		{
-			struct
-			{
-				u8 E;
-				u8 D;
-			};
-			u16 DE;
+			u8 E;
+			u8 D;
 		};
+		u16 DE;
 	};
-	struct
+	union
 	{
-		union
+		struct
 		{
-			struct
-			{
-				u8 L;
-				u8 H;
-			};
-			u16 HL;
+			u8 L;
+			u8 H;
 		};
+		u16 HL;
 	};
 
 	u16 SP;
