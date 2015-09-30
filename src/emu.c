7f34cd1eee3492fd117bd6cbbf6443a59200e508
DylanBelka <dylan.belka@gmail.com>
2015-09-30 18:51:18 -0400

    Add keyboard input, virtual window for future bg scrolling

--- src/emu.c
+++ src/emu.c
@@ -12,6 +12,8 @@ SDL_Window* window;
 SDL_GLContext gl_context;
 bool is_running = true;
 SDL_Surface* window_surf;
+SDL_Surface* virt_window_surf;
+struct GBKeys key_info;
 
 void handle_sdl_error(char *msg)
 {
@@ -31,7 +33,7 @@ void init_video(void)
 		handle_sdl_error("unable to initialize SDL");
 	}
 	window = SDL_CreateWindow("gbemc", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-							  WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+							 WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
 	if (window == NULL)
 	{
 		handle_sdl_error("unable to create window");
@@ -42,6 +44,14 @@ void init_video(void)
 	{
         handle_sdl_error("unable to create window surface");
 	}
+	SDL_PixelFormat *fmt = window_surf->format;
+	virt_window_surf = SDL_CreateRGBSurface(0, VIRT_GB_WINDOW_WIDTH, VIRT_GB_WINDOW_HEIGHT,
+											fmt->BitsPerPixel, fmt->Rmask, fmt->Gmask, fmt->Bmask, fmt->Amask);
+	if (virt_window_surf == NULL)
+	{
+		handle_sdl_error("unable to create virtual window surface");
+	}
+
 	SDL_UpdateWindowSurface(window);
 }
 
@@ -56,8 +66,82 @@ void handle_events(void)
 			is_running = false;
 			break;
 		case SDL_KEYDOWN:
+			{
+			SDL_Keycode key = e.key.keysym.sym;
+			if (key == SDLK_ESCAPE)
+			{
+				is_running = false;
+			}
+			if (key == SDLK_UP) // up
+			{
+				key_info.keys[p14] &= ~keyUp;
+			}
+			if (key == SDLK_DOWN) // down
+			{
+				key_info.keys[p14] &= ~keyDown;
+			}
+			if (key == SDLK_LEFT) // left
+			{
+				key_info.keys[p14] &= ~keyLeft;
+			}
+			if (key == SDLK_RIGHT) // right
+			{
+				key_info.keys[p14] &= ~keyRight;
+			}
+			if (key == SDLK_z) // a
+			{
+				key_info.keys[p15] &= ~keyA;
+			}
+			if (key == SDLK_x) // b
+			{
+				key_info.keys[p15] &= ~keyB;
+			}
+			if (key == SDLK_RETURN) // start
+			{
+				key_info.keys[p15] &= ~keyStart;
+			}
+			if (key == SDLK_BACKSPACE) // select
+			{
+				key_info.keys[p15] &= ~keySelect;
+			}
+			}
 			break;
 		case SDL_KEYUP:
+			{
+			SDL_Keycode key = e.key.keysym.sym;
+			if (key == SDLK_UP) // up
+			{
+				key_info.keys[p14] |= keyUp;
+			}
+			if (key == SDLK_DOWN) // down
+			{
+				key_info.keys[p14] |= keyDown;
+			}
+			if (key == SDLK_LEFT) // left
+			{
+				key_info.keys[p14] |= keyLeft;
+			}
+			if (key == SDLK_RIGHT) // right
+			{
+				key_info.keys[p14] |= keyRight;
+			}
+			if (key == SDLK_z) // a
+			{
+				key_info.keys[p15] |= keyA;
+			}
+			if (key == SDLK_x) // b
+			{
+				key_info.keys[p15] |= keyB;
+			}
+			if (key == SDLK_RETURN) // start
+			{
+				key_info.keys[p15] |= keyStart;
+			}
+			if (key == SDLK_BACKSPACE) // select
+			{
+				key_info.keys[p15] |= keySelect;
+			}
+			}
 			break;
 		}
 	}
@@ -68,6 +152,9 @@ void init(void)
 	reset_mem();
 	reset_cpu();
 	init_video();
+	key_info.keys[0] = 0x0F;
+	key_info.keys[1] = 0x0F;
+	key_info.colID = 0x0;
 }
 
 void run(void)
@@ -104,6 +191,8 @@ void run(void)
 void cleanup(void)
 {
 	SDL_DestroyWindow(window);
+	SDL_FreeSurface(window_surf);
+	SDL_FreeSurface(virt_window_surf);
 	SDL_GL_DeleteContext(gl_context);
 	SDL_Quit();
 }
--- src/emu.h
+++ src/emu.h
@@ -14,6 +14,12 @@
 #define VIRT_GB_WINDOW_WIDTH 256	// 32x32 (tile) screen buffer
 #define VIRT_GB_WINDOW_HEIGHT 256
 
+struct GBKeys
+{
+	u8 keys[2];
+	u8 colID;
+};
+
 void cleanup(void);
 
 void init_gpu(void);
@@ -28,5 +34,24 @@ extern SDL_Window* window;
 extern SDL_GLContext gl_context;
 extern bool is_running;
 extern SDL_Surface* window_surf;
+extern SDL_Surface* virt_window_surf;
+extern struct GBKeys key_info;
+
+enum KeyGroups
+{
+	p14 = 0, p15 = 1
+};
+
+enum KeyCodes
+{
+	keyRight = 0x1,
+	keyA = 0x1,
+	keyLeft = 0x2,
+	keyB = 0x2,
+	keyUp = 0x4,
+	keySelect = 0x4,
+	keyDown = 0x8,
+	keyStart = 0x8
+};
 
 #endif /* GBEMC_EMU_H */
